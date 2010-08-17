e0afb368a16b98b8d5ae3674bbe6bd431fccb3a9
Jay Nemec <jay@ip-89-176-215-114.net.upcbroadband.cz>
2010-08-18 00:05:25 +0200

    Commit 0.1 verze

---
+++ sites/all/libraries/ckeditor/plugins/stylescombo/styles/default.js
@@ -0,0 +1,6 @@
+﻿/*
+Copyright (c) 2003-2009, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.addStylesSet('default',[{name:'Normální',element:'p'},{name:'Poznámka',element:'p',attributes:{'class':'note','id':'0'}},{name:'Literatura',element:'p',attributes:{'class':'ref','id':'0'}}]);
